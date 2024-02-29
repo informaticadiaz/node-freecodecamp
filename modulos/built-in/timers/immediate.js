68f3b9c75583cdfef61d574d92b5187fef2035ce
Ignacio Diaz <informatica.diaz@gmail.com>
2024-02-29 10:39:44 -0300

    .

--- modulos/built-in/timers/immediate.js
+++ modulos/built-in/timers/immediate.js
@@ -6,6 +6,21 @@ Se ejecuta después del código síncrono
 setImmediate(funcion, argumento1, argumento2, ...);
 */
 
+/*
+Ejercicio Practico
+setImmediate() logra que la funcion tenga 
+la maxima prioridad entre todos los eventos pendientes
+osea la maxima prioridad de el codigo asíncrono
+si ejecutas node setImmediate.js
+veras lo siguiente en consola
+Antes de setImmediate
+Despues de setImmediate
+Estoy aprendiendo Node.js
+Esto se debe a que primero se ejecuta 
+el codigo sincrono
+y despues el codigo asincrono
+*/
+
 function mostrarTema(tema) {
     console.log(`Estoy aprendiendo ${tema}`);
 }
---
+++ modulos/built-in/timers/interval.js
@@ -0,0 +1,24 @@
+/*
+setInterval() 
+Se utiliza para ejectar código un número
+infinito de veces con un retraso específico
+de milisegundos
+sintaxis
+todos los argumentos que recibe 
+nos permiten personalizar la funcion
+setInterval(funcion, intervalo, arg1, arg2);
+*/
+
+// Ejercicio Practico
+
+function mostrarTema(tema) {
+    console.log(`Estoy aprendiendo ${tema}`);
+}
+
+setInterval(mostrarTema, 1500, 'Node.js');
+
+function sumar(a, b) {
+    console.log(a + b);
+}
+
+setInterval(sumar, 1500, 10, 20);

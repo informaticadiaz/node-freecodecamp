1a4fe31e2d5e7b059cdb486d53618416b5538252
Ignacio Diaz <informatica.diaz@gmail.com>
2024-02-29 05:42:42 -0300

    .

--- modulos/built-in/process/app.js
+++ modulos/built-in/process/app.js
@@ -5,4 +5,16 @@ También puede tener cierto control
 sobre el proceso.
 */
 
-console.log(process);
+// console.log(process);
+// console.log(process.env);
+// console.log(process.argv);
+// console.log(process.argv[2]);
+// console.log(process.argv[3]);
+
+/*
+for (let i = 2; i < process.argv.length; i++) {
+    console.log(process.argv[i]);
+}
+*/
+
+console.log(process.memoryUsage())

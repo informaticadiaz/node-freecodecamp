588410c1c4ce5016ab3430c2f38b410c18659f0f
Ignacio Diaz <informatica.diaz@gmail.com>
2024-02-28 23:27:34 -0300

    .

--- modulos/saludos/app.js
+++ modulos/saludos/app.js
@@ -1,14 +1,8 @@
 /*
+sintaxis tradicional
+para importar un modulo
 const saludos = require('./saludos');
 */
-
-/*
-sintaxis alternativa de desestructuracion
-para incluir elementos de un modulo
-desestructura el objeto
-*/
-
-const { saludarHolaMundo, saludar } = require('./saludos');
 
 /*
 si miramos el contenido de saludos
@@ -17,14 +11,26 @@ console.log(saludos);
 */
 
 /*
+esta es la sintaxis tradicional
+esta forma de invocar una funcion es mas larga
+ya que debes escribir el nombre del modulo
+mas el nombre de la funcion
 console.log(saludos.saludar('freeCodeCamp'));
 console.log(saludos.saludarHolaMundo());
 */
 
 /*
+La sintaxis alternativa de desestructuracion
+para incluir elementos de un modulo
+desestructura el objeto
+*/
+const { saludarHolaMundo, saludar } = require('./saludos');
+
+/*
 utiliza la sintaxis alternativa
 escribe menos codigo
+ya no tenemos que usar el nombre del modulo
+ahora solo usamos el nombre de la funcion
 */
-
 console.log(saludar('freeCodeCamp'));
 console.log(saludarHolaMundo());

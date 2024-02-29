48b9c08f6bf55189a5e2addc5e0735eeaeb51d04
Ignacio Diaz <informatica.diaz@gmail.com>
2024-02-29 00:07:19 -0300

    .

---
+++ modulos/built-in/console/app.js
@@ -0,0 +1,13 @@
+/*
+métodos utiles de console
+console.log()
+console.warn()
+console.error()
+console.assert()
+console.table()
+*/
+
+console.log('Hello, World!');
+console.warn('Ocurrio un error!');
+console.error(new Error('¡Ocurrio un erro!'));
+
---
+++ modulos/built-in/process/app.js
@@ -0,0 +1,8 @@
+/*
+Provee información sobre el proceso 
+de node que se esta ejecutando.
+También puede tener cierto control
+sobre el proceso.
+*/
+
+console.log(process);

4b23ecd3de9643ffc7b61416bf006000c22612a3
Ignacio Diaz <informatica.diaz@gmail.com>
2024-03-28 14:10:17 -0300

    .

---
+++ js/promesas/altcatch.js
@@ -0,0 +1,25 @@
+// sintaxis alternativa
+const estatusPedido = () => {
+    return Math.random() < 0.8;
+};
+
+const miPedidoDePizza = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if (estatusPedido()) {
+            resolve('¡Tu pedido fue aceptado!');
+        } else {
+            reject('Tu pedido fue rechazado...');
+        }
+    }, 3000);
+});
+const manejarPedido = (mensajeDeConfirmacion) => {
+    console.log(mensajeDeConfirmacion);
+};
+
+const rechazarPedido = (mensajeDeError) => {
+    console.log(mensajeDeError);
+}
+
+miPedidoDePizza
+    .then(manejarPedido)
+    .catch(rechazarPedido);

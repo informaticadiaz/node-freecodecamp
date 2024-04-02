a19c3dba08bc30ec80f078ef80367cca8f43f2e6
Ignacio Diaz <informatica.diaz@gmail.com>
2024-04-01 21:00:03 -0300

    .

--- nodemon/app.js
+++ nodemon/app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.end('Hello FreeCodeCamp!');
+    res.end('Estoy aprendiendo Node.js!');
 });
 
 const PUERTO = 3000;
@@ -9,3 +9,6 @@ const PUERTO = 3000;
 server.listen(PUERTO, () => {
     console.log(`Escuchando en el puerto ${PUERTO}`);
 });
+
+// iniciamos el servidor con nodemon
+// nodemon app.js
